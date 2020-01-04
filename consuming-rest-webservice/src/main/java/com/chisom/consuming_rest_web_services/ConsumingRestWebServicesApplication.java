a9987464ca6e6c261d63b4b14754175929f3c3f9
chisom <iwowochisom@gmail.com>
2020-01-04 19:35:30 +0100

    Initial commit

---
+++ consuming-rest-webservice/src/main/java/com/chisom/consuming_rest_web_services/ConsumingRestWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.chisom.consuming_rest_web_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumingRestWebServicesApplication.class, args);
+	}
+
+}
---
+++ consuming-rest-webservice/src/test/java/com/chisom/consuming_rest_web_services/ConsumingRestWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chisom.consuming_rest_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
