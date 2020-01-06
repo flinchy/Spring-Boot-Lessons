c20ec7caa81c8c922a09836351a8b23bc2df422c
chisom <iwowochisom@gmail.com>
2020-01-07 00:01:58 +0100

    create POJO for message carrying name

---
+++ messaging-websocket/src/main/java/com/chisom/messaging_websocket/hello/HelloMessage.java
@@ -0,0 +1,21 @@
+package com.chisom.messaging_websocket.hello;
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
