7aaaca4c8146d0b011227ebcdb82557fc8b636e5
chisom <iwowochisom@gmail.com>
2020-01-07 00:05:38 +0100

    create POJO Greeting to process HelloMessage

---
+++ messaging-websocket/src/main/java/com/chisom/messaging_websocket/hello/Greeting.java
@@ -0,0 +1,21 @@
+package com.chisom.messaging_websocket.hello;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
