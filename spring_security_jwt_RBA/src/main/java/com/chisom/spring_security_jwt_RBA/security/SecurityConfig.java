39ebb515c4730d2163dd9afc9c0102bc6a35ed74
chisom <iwowochisom@gmail.com>
2020-01-08 15:57:08 +0100

    Configure WebSecurityAdapter

--- spring_security_jwt_RBA/src/main/java/com/chisom/spring_security_jwt_RBA/security/SecurityConfig.java
+++ spring_security_jwt_RBA/src/main/java/com/chisom/spring_security_jwt_RBA/security/SecurityConfig.java
@@ -1,7 +1,51 @@
 package com.chisom.spring_security_jwt_RBA.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+@EnableGlobalMethodSecurity( // For Role based Authorization in the future
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true
+)
+
+/**
+ * WebSecurityConfigurerAdapter provides some default security configurations what we are doing by extending it is that it is
+ * going to allow us to customize those security configurations by overriding some of its methods
+ */
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .headers().frameOptions().sameOrigin()// For H2 Database
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        "/",
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js"
+                ).permitAll()
+                .anyRequest().authenticated();
+    }
 }
