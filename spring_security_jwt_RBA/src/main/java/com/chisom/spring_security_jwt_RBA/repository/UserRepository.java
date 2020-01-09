bf185d03d0e431c909c3ed22bc01130d40d9a74d
chisom <iwowochisom@gmail.com>
2020-01-09 08:27:59 +0100

    Modify UserRepository

--- spring_security_jwt_RBA/src/main/java/com/chisom/spring_security_jwt_RBA/repository/UserRepository.java
+++ spring_security_jwt_RBA/src/main/java/com/chisom/spring_security_jwt_RBA/repository/UserRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
+//    Optional<User> findByUsername(String username);
     User getById(Long id);
 }
