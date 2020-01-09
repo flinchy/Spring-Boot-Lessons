80e96816b6f2422fb8880704871a4d269ca999a1
chisom <iwowochisom@gmail.com>
2020-01-09 19:28:53 +0100

    Modify hello controller

--- spring_security_jwt_RBA/src/main/java/com/chisom/spring_security_jwt_RBA/web/HelloController.java
+++ spring_security_jwt_RBA/src/main/java/com/chisom/spring_security_jwt_RBA/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.chisom.spring_security_jwt_RBA.web;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +14,15 @@ public class HelloController {
         return "Hello youtube";
     }
 
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @GetMapping("/secured/all")
     public String securedHello() {
         return "Welcome to the secured page saying hello";
+    }
+
+    @PreAuthorize("hasAnyRole('ACTUATOR')")
+    @GetMapping("/secured/actuator")
+    public String securedActuator() {
+        return "Welcome Actuator";
     }
 }
