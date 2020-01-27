cf7b95ad3eb34b361a2ce081344fd7ff169fbed4
chisom <iwowochisom@gmail.com>
2020-01-27 20:41:55 +0100

    Add README

---
+++ quartz-demo-email-scheduling-app/src/test/java/com/example/quartzdemoemailschedulingapp/QuartzDemoEmailSchedulingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quartzdemoemailschedulingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzDemoEmailSchedulingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
